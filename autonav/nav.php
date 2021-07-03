44a3c06d7d7ff519660b040519c51d8851de2ec2
Polygon <79771454+PoIygon@users.noreply.github.com>
2021-07-03 20:12:48 +1000

    new stu

---
+++ autonav/nav.php
@@ -0,0 +1,7 @@
+<nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/password/">Login</a></li>
+            <li><a href="/Signup/">Sign Up</a></li>
+        </ul>
+</nav>
