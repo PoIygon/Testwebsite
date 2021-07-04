af5879564b1cc434100e182052e3a93dd7b4aeb4
Polygon <79771454+PoIygon@users.noreply.github.com>
2021-07-05 09:53:11 +1000

    i finally made a non-static website!

--- userinfo/fetch.js
+++ userinfo/fetch.js
@@ -1,16 +1,25 @@
-const wrapper = document.querySelector('.wrapper');
-
+const wrapper = document.querySelector('#myData');
+    
 function createContent(data) {
-  const content = `
-    <img src="${data.images}" alt="PoIygon" width="50">
-    <span>${data.username}</span>
-    <br>
-    <span>${data.country}</span>
-  `;
-  wrapper.insertAdjacentHTML('beforeend', content);
+const content = `
+<div class="down">
+    <div class="right">
+        <img src="https://cdn2.scratch.mit.edu/get_image/user/${data.id}_500x500.png" alt="" width="100">
+        <div class="down">
+            <h2>${data.username}</h2>
+            <span>country: ${data.country} | joined: ${data.joined} <br>${data.status}</span>
+        </div>
+    </div>
+    <div class="right">
+        BIO:<br>${data.bio}<br>
+        what im working:<br>${data.work}
+    </div>
+</div>
+`;
+wrapper.insertAdjacentHTML('beforeend', content);
 }
 
-fetch('https://api.scratch.mit.edu/users/poiygon')
-  .then(response => response.json())
-  .then(data => createContent(data))
-  .catch((error) => console.log(error));
+fetch('https://scratchdb.lefty.one/v3/user/info/PoIygon')
+.then(response => response.json())
+.then(data => createContent(data))
+.catch((error) => console.log(error));
